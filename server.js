d82f8f96c1e2b20f2d9341998a66111fbe09554b
adam zier <blueflash2o@gmail.com>
2017-06-27 20:28:21 -0500

    Make it fill out the form correctly.
    Add comments.

--- server.js
+++ server.js
@@ -12,21 +12,24 @@ app.get('/crawl', function (req, res) {
         obj.sites.forEach(function (element) {
             request(element.url, function (error, response, html) {
                 fillform(html, element);
-
             });
         }, this);
     });
     res.send('Check your console!');
 })
 
-app.listen('8081')
+app.listen('8081');
 
-console.log('Listen on port 8081');
+console.log('Listen for port 8081');
 
 exports = module.exports = app;
 
+/**
+ * fill out a given form with the object data.
+ * @param {*} html the html form 
+ * @param {*} site the site data that holds forms.
+ */
 function fillform(html, site) {
-    //console.log(site);
     request.debug = true;
     var $ = cheerio.load(html);
     var origin = site.origin;
@@ -34,31 +37,23 @@ function fillform(html, site) {
         console.log(form);
         var postAction = $('form').attr('action');
         var url = `${origin}/${postAction}`;
-        // var formData = new FormData();
-        // form.inputs.forEach(function(input){
-        //     formData.append(input.id, input.value);
-        // });
         request.post({ url: url, form: form.inputs }, function optionalCallback(err, httpResponse, body) {
             if (err) {
                 return console.error('upload failed:', err);
             }
-            //console.log('Upload successful!  Server responded with:', body);
             scrapper(body, form);
         });
     })
-    console.log($('form').attr('action'));
-    var forms = $('forms');
-    //console.log(forms);
 }
 
+/**
+ * Scrap html and pull out data. Logs h3 attributes
+ * @param {*} html the html to scrape
+ * @param {*} form a form that would have the needed data.
+ */
 function scrapper(html, form) {
     var $ = cheerio.load(html);
     var h3 = $('h3');
     var rc = h3.attr("class");
     console.log(`${h3.attr("class")} response ${h3.text()}`);
-    // if(rc == 'success'){
-    //     console.log(`success response ${h3.text()}`);
-    // } else if( rc == 'error'){
-    //     console.log(`error response ${h3.text()}`);
-    // }
 }
